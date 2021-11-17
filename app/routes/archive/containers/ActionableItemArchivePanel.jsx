422caed39482027741d2253dc4cc41490101a8db
mccannsa <smccann144@gmail.com>
2021-11-17 17:59:16 -0500

    running moss on cloneDestination, but an already existing clone

--- app/routes/archive/containers/ActionableItemArchivePanel.jsx
+++ app/routes/archive/containers/ActionableItemArchivePanel.jsx
@@ -6,8 +6,14 @@ import { connect } from "react-redux"
 
 import ItemArchivePanel from "../components/ItemArchivePanel"
 import { submissionViewDirectory } from "../../../modules/submissions/actions/submission-view-directory"
+import { cloneDestination } from "../../../modules/settings/selectors"
 
-const mapStateToProps = (state, ownProps) => ownProps
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ownProps,
+    destination: cloneDestination(state)
+  }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
--- app/routes/archive/index.jsx
+++ app/routes/archive/index.jsx
@@ -11,6 +11,8 @@ import ArchiveProgressPanel from "./containers/ArchiveProgressPanel"
 import { settingsResetState } from "../../modules/settings/actions/settings-reset-state"
 import { progress } from "../../modules/submissions/selectors"
 
+import { cloneDestination } from "../../modules/settings/selectors"
+
 const methods = {
   componentDidMount (_props) {
   }
@@ -28,13 +30,26 @@ const forwardButton = (progress, quitApp) => {
   }
 }
 
-const mossButton = (progress, quitApp) => {
+const mossButton = (progress, destination) => {
   if (progress < 0 || progress === 100) {
     return (
       {
         label: "Run MOSS on files",
-        route: "/populate",
-        onClick: quitApp
+        route: "/archive",
+        onClick: async function (state) {
+          const util = require('util')
+          const exec = util.promisify(require('child_process').exec)
+          const child = await exec(`./app/routes/archive/moss -l java ${destination}/hello-world-11-17-2021-04-41-32/*/*.java ${destination}/hello-world-11-17-2021-04-41-32/*/*.java >> ./app/routes/archive/output.moss`,
+              function (error, stdout, stderr) {
+                window.alert(stdout)
+                  // console.log('stdout: ' + stdout);
+                  // console.log('stderr: ' + stderr);
+                  if (error !== null) {
+                      window.alert('exec error: ' + error);
+                  }
+              });
+          child();
+        }
       }
     )
   }
@@ -43,11 +58,12 @@ const mossButton = (progress, quitApp) => {
 
 const ArchivePage = ({
   quitApp,
-  progress
+  progress,
+  cloneDestination
 }) => (
   <div>
     <AssignmentPanel/>
-    <ArchiveProgressPanel progress={progress}/>
+    <ArchiveProgressPanel progress={progress} cloneDestination={cloneDestination}/>
     <SubmissionArchivePanelList />
     <NavFooter
       left={{
@@ -57,13 +73,14 @@ const ArchivePage = ({
         disabled: progress < 0 || progress === 100
       }}
       right= {forwardButton(progress, quitApp)}
-      center= {mossButton(progress, quitApp)}
+      center= {mossButton(progress, cloneDestination)}
     />
   </div>
 )
 
 const mapStateToProps = (state) => ({
-  progress: progress(state)
+  progress: progress(state),
+  cloneDestination: cloneDestination(state)
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -74,7 +91,8 @@ const mapDispatchToProps = (dispatch) => ({
 
 ArchivePage.propTypes = {
   quitApp: PropTypes.func.isRequired,
-  progress: PropTypes.number
+  progress: PropTypes.number,
+  cloneDestination: PropTypes.string
 }
 
 export default lifecycle(methods)(connect(mapStateToProps, mapDispatchToProps)(ArchivePage))
